0e8778e937f55f26b141c30236b9dc13f35a7724
Student <Student@B77>
2018-11-27 09:48:41 +0800

    testing as

--- src/testing/Testing.java
+++ src/testing/Testing.java
@@ -17,6 +17,9 @@ public class Testing {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Testing");
+        System.out.println("Testing");
+        System.out.println("Testing");
+        
     }
     
 }
